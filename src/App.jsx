1c27ad6dfb398ee7749a7bfd0d1fec877b2bae95
Monika19727 <71118507+Monika19727@users.noreply.github.com>
2025-02-28 21:39:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<UserLayout/>}>
+      <Route path="/" element={<UserLayout/>}>  //routing for userlayout
       </Route>
     </Routes>
     </BrowserRouter>
